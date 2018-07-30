3501d59904093b3b5bb0b23609115011e252be77
Lourens Dijkstra <lourens.dijkstra@gmail.com>
2018-07-30 14:53:30 +0200

    initial commit

---
+++ is_number.h
@@ -0,0 +1,12 @@
+namespace master_parser {
+    template <char C0, char... Cs> // if Cs is empty?
+    struct is_number  // ..
+        : std::conditional_t<(is_number<C0>::value) || C0 == '.'
+            , is_number<Cs...>, std::false_type>
+    {};
+
+    template <char C>
+    struct is_number<C>
+        : std::conditional_t<(C >= '0' && C <= '9'), std::true_type, std::false_type>
+    {};
+}
