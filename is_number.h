bf10a271de2cc5c3f3382e1cda8c8db9e10babfd
Lourens Dijkstra <lourens.dijkstra@gmail.com>
2018-08-10 20:43:04 +0200

    Update is_number.h

--- is_number.h
+++ is_number.h
@@ -1,12 +1,14 @@
 namespace parser {
-    template <char C0, char... Cs>
-    struct is_number  // ..
-        : std::conditional_t<(is_number<C0>::value) || C0 == '.'
-            , is_number<Cs...>, std::false_type>
-    {};
+    namespace variadic {
+        template <char C0, char... Cs>
+        struct is_number  // ..
+            : std::conditional_t<(is_number<C0>::value) || C0 == '.'
+                , is_number<Cs...>, std::false_type>
+        {};
 
-    template <char C>
-    struct is_number<C>
-        : std::conditional_t<(C >= '0' && C <= '9'), std::true_type, std::false_type>
-    {};
+        template <char C>
+        struct is_number<C>
+            : std::conditional_t<(C >= '0' && C <= '9'), std::true_type, std::false_type>
+        {};
+    }
 }
